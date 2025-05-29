764548ac9c674f13cad6cceeaf062cd456d40a0f
iym6 <iym6@yandex.ru>
2025-05-29 15:03:17 +0700

    фикс бд

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 return [
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'mvc_2',
+    'database' => 'mvc',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
